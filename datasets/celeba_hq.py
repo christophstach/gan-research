d25f947bdb929f4ee40c0c2390d746421485ac32
Christoph Stach <christoph.stach@gmail.com>
2020-07-11 21:27:25 +0800

    wip

--- datasets/celeba_hq.py
+++ datasets/celeba_hq.py
@@ -1,9 +1,9 @@
 from torchvision.datasets.folder import default_loader
 
-from gans.datasets.flat_image_folder import FlatImageFolder
+from .flat_image_folder import FlatImageFolder
 
 
 class CelebAHQ(FlatImageFolder):
-    def __init__(self, root, image_size=1024, transform=None, loader=default_loader):
+    def __init__(self, root, image_size=1024, transform=None, loader=default_loader, train=True, download=True):
         root += "/celebAHQ/data" + str(image_size) + "x" + str(image_size)
         super().__init__(root, transform, loader)
